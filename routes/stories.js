e25c4114038e3ce640b58bb5f27f00995164a7b5
Alex M <alexandru.manoliu@gmail.com>
2017-12-04 22:45:18 +0200

    add route to stories show

--- routes/stories.js
+++ routes/stories.js
@@ -16,6 +16,18 @@ router.get('/', (req, res) => {
     });
 });
 
+//show single story
+router.get('/show/:id', (req, res) => {
+Story.findOne({
+  _id:req.params.id
+})
+ .then(story => {
+  res.render('stories/show', {
+    story: story
+  });
+ });
+});
+
 // Add Story Form
 router.get('/add', ensureAuthenticated, (req, res) => {
 
