a87e2a17d5785ef1fe624a18ad9c4d737c9edcdc
Alex M <alexandru.manoliu@gmail.com>
2017-12-06 17:28:58 +0200

    versione 1.0.0alpha done

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:'mongodb://alexmnl:parola@ds129936.mlab.com:29936/storybook-db',
-  googleClientID:'37433702721-51lfg9dms843smed69slt4q6t4m1ii6i.apps.googleusercontent.com',
-  googleClientSecret:'JULE8BClPfgLydXRE1643RfY'
+if(process.env.NODE_ENV === 'production'){
+ module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
