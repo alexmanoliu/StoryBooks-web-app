81656fc65bc7ac6d9846a6b0edce9f8dddb65b69
Alex M <alexandru.manoliu@gmail.com>
2017-12-06 11:56:05 +0200

    added comments edit option

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
@@ -42,9 +43,12 @@ mongoose.connect(keys.mongoURI, {
 
 const app = express();
 
-//parse application/json
+//Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//method override middleware
+app.use(methodOverride('_method'));
 
 //handlebars middleware
 app.engine('handlebars', exphbs({
--- routes/stories.js
+++ routes/stories.js
@@ -73,4 +73,58 @@ router.post('/', (req, res) => {
     });
 });
 
+//edit form process
+router.put('/:id', (req, res) => {
+  Story.findOne({
+    _id:req.params.id
+  })
+   .then(story => {
+    let allowComments;
+    
+      if(req.body.allowComments){
+        allowComments = true;
+      } else {
+        allowComments = false;
+      }
+    // new values
+    story.title = req.body.title;
+    story.body = req.body.body;
+    story.status = req.body.status;
+    story.allowComments = allowComments;
+
+    story.save()
+    .then(story => {
+      res.redirect('/dashboard');
+    });
+   });
+});
+
+//delete story
+router.delete('/:id', (req, res) => {
+  Story.remove({_id: req.params.id})
+  .then(() => {
+    res.redirect('/dashboard');
+  });
+});
+
+//add comment
+router.post('/comment/:id', (req, res) => {
+  Story.findOne({
+    _id: req.params.id
+  })
+  .then(story => {
+    const newComment = {
+      commentBody: req.body.commentBody,
+      commentUser: req.user.id
+    }
+
+    //add to comments array
+    story.comments.unshift(newComment);
+
+    story.save()
+    .then(story => {
+      res.redirect(`/stories/show/${story.id}`);
+    });
+  });
+});
 module.exports = router;
